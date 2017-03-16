da196b8d21929e71a2db7ddeaa93e39175770274
Ferney F Roa H <ferneyfroah@MacBook-Pro-de-Ferney.local>
2017-03-16 13:54:46 -0500

    Initial Commit

---
+++ Hola Mundo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hola Mundo
+//
+//  Created by Ferney F Roa H on 16/03/17.
+//  Copyright © 2017 swael. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
